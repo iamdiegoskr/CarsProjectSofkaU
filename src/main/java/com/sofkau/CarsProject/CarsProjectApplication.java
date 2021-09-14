3932f3651a91f46c3f4918afe605fc89fce231a5
Diego Martinez <juandiego337@hotmail.com>
2021-09-14 12:57:15 -0500

    Initial config is ready

---
+++ src/main/java/com/sofkau/CarsProject/CarsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.CarsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/CarsProject/CarsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.CarsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
